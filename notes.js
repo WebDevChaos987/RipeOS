2dfd76c6e118f6399335f2e49a9293facf2e686c
WebDevChaos987 <avery.mancini.2016@gmail.com>
2026-05-10 19:21:18 +0800

    Create notes.js with project information
    Added introductory notes about RipeOS. Also, I added a quick side note on Linux Mint

---
+++ notes.js
@@ -0,0 +1,13 @@
+/*Welcome to, RipeOS. A virtual Operating System, in it's beta.
+Also, this is an open source project, with an MIT License, as you may have seen.
+This Virtual Operating System, is not available for Mobile Users
+(and probably never will be, as it's not target at them). Recently,
+you could have seen, that people have been switching from,
+Microsoft Windows, to a Linux Distro, Linux Mint. And I definitly,
+agree; If you use Windows, you should try Linux Mint. There are NO constant
+updates, and it feels like windows (e.g. the Linux Mint Pannel).
+
+Anyways, these are all the notes for now
+*/
+
+alert("BOO!");
